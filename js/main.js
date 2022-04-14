a8610cef91384d80a836a25776f2b40bdad80247
Abrar-Muftah <6021888@mborijnland.nl>
2022-04-14 09:41:21 +0200

    808

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
+let arrayfibo = [];
+
 const MatheMate = {
-  addition:       function (query1, query2) {
+  addition: function (query1, query2) {
     return query1 + query2;
   },
-  subtraction:    function (query1, query2) {
+  subtraction: function (query1, query2) {
     return query1 - query2;
   },
   multiplication: function (query1, query2) {
     return query1 * query2;
   },
-  division:       function (query1, query2) {
+  division: function (query1, query2) {
     return query1 / query2;
   },
-  power:          function (query1, query2) {
+  power: function (query1, query2) {
     return query1 ** query2;
   },
-  percantage:     function (query1, query2) {
+  percantage: function (query1, query2) {
     return (query1 / 100) * query2;
   },
-  rest:           function (query1, query2) {
+  rest: function (query1, query2) {
     return query1 % query2;
   },
-  fibonacci:      function (query1) {
+  fibonacci: function (query1) {
     const number = parseInt(query1);
     let n1 = 0,
       n2 = 1,
@@ -33,11 +35,24 @@ const MatheMate = {
       n1 = n2;
       n2 = nextTerm;
       nextTerm = n1 + n2;
+      fibnums.push(nextTerm);
     }
-    fibnums.push(nextTerm);
-    return nextTerm;
+    // return nextTerm;
   },
-  prime:          function (num) {
+  fibo: function (totfibo) {
+    let antwoordfibo = 0;
+    let arrayfibo = [1, 1];
+    console.log(totfibo);
+    for (let i = 0; antwoordfibo < totfibo; i++) {
+      let nummerfibo1 = arrayfibo[i];
+      let nummerfibo2 = arrayfibo[i + 1];
+      antwoordfibo = nummerfibo1 + nummerfibo2;
+      arrayfibo[arrayfibo.length] = antwoordfibo;
+      console.log(arrayfibo);
+    }
+    return arrayfibo;
+  },
+  prime: function (num) {
     for (let i = 0; i <= num; i++) {
       let input = 0;
 
@@ -47,13 +62,13 @@ const MatheMate = {
         }
       }
 
-    //   if number greater than 1 and not divisible by other numbers
-        if (i > 1 && input == 0) {
-          console.log(i);
-        }
+      //   if number greater than 1 and not divisible by other numbers
+      if (i > 1 && input == 0) {
+        console.log(i);
+      }
     }
   },
-  tafel:          function (to, table) {
+  tafel: function (to, table) {
     let tabelList = [];
     for (let i = 0; i <= to; i++) {
       let ans = (table[i] = i * table);
@@ -62,22 +77,23 @@ const MatheMate = {
     }
     return tabelList;
   },
-  breuken:        function (query1) {
-    let ans = [];
-    for (let i = 0; i < query1; i++) {
-      ans = 1 / [i];
+  breuken: function (query1) {
+    for (let i = 1; i <= query1; i++) {
+      let ans = "1 / " + i + " = " + MatheMate.division(1, i);
+
+      arrayAns.push(ans);
     }
-    return ans;
+    return arrayAns;
   },
-  evenoneven:     function (num) {
+  evenoneven: function (num) {
     if (num % 2 == 0) {
-      num += " is een even getal";
+      num = " even getal";
     } else {
-      num += " is an oneven getal";
+      num = " oneven getal";
     }
     return num;
   },
-  romanizer:      function (num) {
+  romanizer: function (num) {
     if (isNaN(num)) return NaN;
     let digits = String(+num).split(""),
       key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
@@ -88,6 +104,31 @@ const MatheMate = {
   },
 };
 
+
+document.getElementById("antwoordfibobutton").addEventListener("click", fibo);
+// const MathComplex = {
+//   fibo: function (totfibo) {
+//     let antwoordfibo = 0;
+//     let arrayfibo = [1, 1];
+//     console.log(totfibo);
+//     for (let i = 0; antwoordfibo < totfibo; i++) {
+//       let nummerfibo1 = arrayfibo[i];
+//       let nummerfibo2 = arrayfibo[i + 1];
+//       antwoordfibo = nummerfibo1 + nummerfibo2;
+//       arrayfibo[arrayfibo.length] = antwoordfibo;
+//       console.log(arrayfibo);
+//     }
+//     return arrayfibo;
+//   },
+// };
+
+// console.log(MatheMate.fibonacci(50));
+
+// MatheMate.fibonacci(50);
+
+// fibnums.forEach((e) => {
+//   console.log(e);
+// });
 
 // addition
 // subtraction
@@ -101,4 +142,4 @@ const MatheMate = {
 // breuken
 // evenoneven
 // romanizer
-// prime 
+// prime
--- js/push.js
+++ js/push.js
@@ -1,47 +1,83 @@
+// ADDITION
 document.querySelector("#additionBtn").addEventListener("click", ()=> {
     document.querySelector("#additionAns").innerHTML = MatheMate.addition(
         parseInt(document.querySelector("#addition1").value),
         parseInt(document.querySelector("#addition2").value)
     )
 });
+
+// SUBTRACTION
 document.querySelector("#subtractBtn").addEventListener("click", ()=> {
     document.querySelector("#subtractAns").innerHTML = MatheMate.subtraction(
         parseInt(document.querySelector("#subtract1").value),
         parseInt(document.querySelector("#subtract2").value)
     )
 });
+
+// MULTIPLICATION
 document.querySelector("#multiplyBtn").addEventListener("click", ()=> {
     document.querySelector("#multiplyAns").innerHTML = MatheMate.multiplication(
         parseInt(document.querySelector("#multiply1").value),
         parseInt(document.querySelector("#multiply2").value)
     )
 });
+
+// DIVISION
 document.querySelector("#divisionBtn").addEventListener("click", ()=> {
     document.querySelector("#divisionAns").innerHTML = MatheMate.division(
         parseInt(document.querySelector("#division1").value),
         parseInt(document.querySelector("#division2").value)
     )
 });
+
+// PERCENTAGE
 document.querySelector("#percentageBtn").addEventListener("click", ()=> {
     document.querySelector("#percentageAns").innerHTML = MatheMate.percantage(
         parseInt(document.querySelector("#percentage1").value),
         parseInt(document.querySelector("#percentage2").value)
     )
 });
+
+// REST WARDE
 document.querySelector("#restBtn").addEventListener("click", ()=> {
     document.querySelector("#restAns").innerHTML = MatheMate.rest(
         parseInt(document.querySelector("#rest1").value),
         parseInt(document.querySelector("#rest2").value)
     )
 });
+
+// POWER
 document.querySelector("#powerBtn").addEventListener("click", ()=> {
     document.querySelector("#powerAns").innerHTML = MatheMate.power(
         parseInt(document.querySelector("#power1").value),
         parseInt(document.querySelector("#power2").value)
     )
 });
+
+// FIBONACCI
 document.querySelector("#fibonacciBtn").addEventListener("click", ()=> {
-   document.getElementById("fibonacciAns").innerHTML = MatheMate.fibonacci(
-        document.getElementById("fibonacci1").value
+    arrayfibo = MatheMate.fibo(document.getElementById("fibonacci1").value);
+    for (let i = 0; i < arrayfibo.length - 1; i++) {
+        document.getElementById("fibonacciAns").innerHTML += arrayfibo[i] + ", ";
+    }
+   
+});
+
+// EVEN ONEVEN
+document.querySelector("#evenBtn").addEventListener("click", ()=> {
+   document.getElementById("evenAns").innerHTML = MatheMate.evenoneven(
+        document.getElementById("evenoneven").value
    )
 });
+
+
+// BREUKEN
+
+
+
+// ROMANIZER
+document.querySelector("#romanizerBtn").addEventListener("click", ()=> {
+    document.getElementById("romanizerAns").innerHTML = MatheMate.romanizer(
+         parseInt(document.getElementById("romanizer1").value)
+    )
+ });
